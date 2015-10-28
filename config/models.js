2f0b90aabaee1d0b4c5a9fafe559f6c4b8275372
Vasiliy Makarov <drmoriarty.0@gmail.com>
2015-10-28 12:06:56 +0300

    change db to mongo

--- config/models.js
+++ config/models.js
@@ -17,8 +17,8 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-    connection: 'localDiskDb',
-    //connection: 'someMongodbServer',
+    //connection: 'localDiskDb',
+    connection: 'someMongodbServer',
 
   /***************************************************************************
   *                                                                          *
